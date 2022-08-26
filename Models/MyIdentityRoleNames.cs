d52d5f27bf4d68fc83f8bf3782bc4a73856352bd
mulip <mulip@BSBLRL-12717>
2022-08-26 22:49:49 +0530

    Add project files.

---
+++ Models/MyIdentityRoleNames.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Web.Models
+{
+    public enum MyIdentityRoleNames
+    {
+
+        [Display(Name = "Admin Role")]
+        AppAdmin,
+
+        [Display(Name = "Librarian")]
+        AppLibrarian,
+        
+        [Display(Name = "User Role")]
+        AppUser
+
+    }
+}
